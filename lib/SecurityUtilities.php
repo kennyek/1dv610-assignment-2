7e4573ffba4ba83ae3bc83654ed5b3d8b18b7e02
Kenny Ek <ekkenny@hotmail.com>
2018-11-19 20:51:16 +0100

    Remove comments for cleaner code

--- lib/SecurityUtilities.php
+++ lib/SecurityUtilities.php
@@ -1,16 +1,9 @@
 <?php
 
-/** Set of static methods to use for security purposes. */
 class SecurityUtilities
 {
     private static $unsafeCharacters = ['<', '>', '/'];
 
-    /**
-     * Creates a random string of a specific lenght.
-     *
-     * @param int $length (optional) - The length of the string. Defaults to 30.
-     * @return string A random string of a specific length.
-     */
     public static function createRandomStringOfLength(int $length = 30): string
     {
         $characters =
@@ -31,14 +24,6 @@ class SecurityUtilities
         return $randomString;
     }
 
-    /**
-     * Creates an encrypted version of a string. Can be used to encrypt
-     * sensitive data such as passwords. Use password_verify to check the plain
-     * string against the encrypted string.
-     *
-     * @param string $unencryptedString - The string to encrypt.
-     * @return string An encrypted version of the provided string.
-     */
     public static function encryptString(string $unencryptedString): string
     {
         $algorithm = PASSWORD_BCRYPT;
@@ -51,12 +36,6 @@ class SecurityUtilities
         return $hash;
     }
 
-    /**
-     * Checks whether or not the provided string has unsafe characters in it.
-     *
-     * @param string $toAnalyze - The string to analyze for unsafe characters.
-     * @return boolean Whether or not the provided string has unsafe characters.
-     */
     public static function hasUnsafeCharacters(string $toAnalyze)
     {
         $hasUnsafeCharacters = false;
@@ -71,14 +50,6 @@ class SecurityUtilities
         return $hasUnsafeCharacters;
     }
 
-    /**
-     * Creates a copy of the string with unsafe characters, such as angle
-     * brackets, removed.
-     *
-     * @param string $unsafe - The string to remove unsafe characters from.
-     * @return string A copy of the passed string with unsafe characters
-     * removed.
-     */
     public static function removeUnsafeCharactersFromString(string $unsafe): string
     {
         return strip_tags($unsafe);
