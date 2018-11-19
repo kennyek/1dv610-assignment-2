41218ccfda24c53c1c0a4a146da54349300e4d4c
Kenny Ek <ekkenny@hotmail.com>
2018-11-19 20:46:10 +0100

    Fix bug with ordinal and make code clean

--- views/DateTimeView.php
+++ views/DateTimeView.php
@@ -2,22 +2,87 @@
 
 class DateTimeView
 {
-    /** Renders a paragraph describing current date and time. */
-    public function show()
+    /** @var DateTime */
+    private $currentDateTime;
+
+    public function __construct(string $timeZone = 'Europe/Stockholm')
     {
-        $currentDateTime = new DateTime('now', new DateTimeZone('Europe/Stockholm'));
-        $dayOfWeek = $currentDateTime->format('l');
-        $dayOfMonth = $currentDateTime->format('j');
-        $monthAsText = $currentDateTime->format('F');
-        $year = $currentDateTime->format('Y');
-        $hour = $currentDateTime->format('H');
-        $minutes = $currentDateTime->format('i');
-        $seconds = $currentDateTime->format('s');
+        $dateTimeZone = new DateTimeZone($timeZone);
+        $this->currentDateTime = new DateTime('now', $dateTimeZone);
+    }
 
-        $timeDescription =
-            "$dayOfWeek, the $dayOfMonth" . "th of $monthAsText $year, " .
-            "The time is $hour:$minutes:$seconds";
+    public function getHtml(): string
+    {
+        return "<p>" . $this->getTimeDescription() . "</p>";
+    }
 
-        return "<p>$timeDescription</p>";
+    private function getTimeDescription(): string
+    {
+        $dayOfWeek = $this->getDayOfWeek();
+        $dayOfMonth = $this->getDayOfMonth();
+        $ordinal = $this->getOrdinal($dayOfMonth);
+        $monthName = $this->getMonthName();
+        $year = $this->getYear();
+
+        $hour = $this->getHour();
+        $minute = $this->getMinute();
+        $second = $this->getSecond();
+
+        return
+            "$dayOfWeek, the $dayOfMonth" . "$ordinal of $monthName $year, " .
+            "The time is $hour:$minute:$second";
+    }
+
+    private function getDayOfWeek(): string
+    {
+        return $this->currentDateTime->format('l');
+    }
+
+    private function getDayOfMonth(): string
+    {
+        return $this->currentDateTime->format('j');
+    }
+
+    private function getOrdinal(int $dayOfMonth): string
+    {
+        switch ($dayOfMonth) {
+            case 1:
+            case 21:
+            case 31:
+                return 'st';
+            case 2:
+            case 22:
+                return 'nd';
+            case 3:
+            case 23:
+                return 'rd';
+            default:
+                return 'th';
+        }
+    }
+
+    private function getMonthName(): string
+    {
+        return $this->currentDateTime->format('F');
+    }
+
+    private function getYear(): string
+    {
+        return $this->currentDateTime->format('Y');
+    }
+
+    private function getHour(): string
+    {
+        return $this->currentDateTime->format('H');
+    }
+
+    private function getMinute(): string
+    {
+        return $this->currentDateTime->format('i');
+    }
+
+    private function getSecond(): string
+    {
+        return $this->currentDateTime->format('s');
     }
 }
